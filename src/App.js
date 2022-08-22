e0757be8c16d3180a6816fa269c209c1b0009830
Kang-zh <paris267@gmail.com>
2022-08-23 02:00:47 +0800

    練習 Routes, Route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
-import {
-  HashRouter,
-  NavLink,
-} from 'react-router-dom';
+import { BrowserRouter, NavLink, Routes, Route } from 'react-router-dom';
 
 const Todo = () => {
   return <p>這是 Todo 頁面 
@@ -17,25 +14,30 @@ const Register = () => {
 
 function App() {
   return (
-    <div className="container">
-      <HashRouter>
-        <div className="nav-link">
-          <NavLink to="/">
+    <div className='container'>
+      <BrowserRouter>
+        <div className='nav-link'>
+          <NavLink to='/'>
             <p>回到首頁</p>
           </NavLink>
-          <NavLink to="/register">
+          <NavLink to='/register'>
             <p>註冊頁面</p>
           </NavLink>
-          <NavLink to="/login">
+          <NavLink to='/login'>
             <p>登入頁面</p>
           </NavLink>
-          <NavLink to="/todo">
+          <NavLink to='/todo'>
             <p>Todo 頁面</p>
           </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
+        <Routes>
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/todo' element={<Todo />} />
+        </Routes>
         {/* 練習區 */}
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
